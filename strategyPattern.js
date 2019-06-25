981816351bd9c1da05067b693bef308a83bbf115
hcxowe <hcxowe@126.com>
2019-06-25 10:52:25 +0800

    策略模式

--- strategyPattern.js
+++ strategyPattern.js
@@ -68,3 +68,46 @@ Animate.prototype.step = function() {
 Animate.prototype.update = function(pos) {
     this.dom.style[this.propertyName] = pos + 'px'
 }
+
+/* 策略模式校验表单 */
+var vaildMethod = {
+    isNoEmpty: function(value, errorMsg) {
+        if (value === '') {
+            return errorMsg
+        }
+    },
+    minLength: function(value, length, errorMsg) {
+        if (value.length < length) {
+            return errorMsg
+        }
+    },
+    isMobile: function(value, errorMsg) {
+        if (!/^1[3|5|8|9][0-9]{9}$/.test(value)) {
+            return errorMsg
+        }
+    }
+}
+
+var Validator = function() {
+    this.cache = []
+}
+
+Validator.prototype.add = function(dom, rule, errorMsg) {
+    var ary = rule.split(':')
+    this.cache.push(function() {
+        var method = ary.shift()
+        ary.unshift(dom.value)
+        ary.push(errorMsg)
+
+        return vaildMethod[method].apply(dom, ary)
+    })
+}
+
+Validator.prototype.start = function() {
+    for (var i = 0, validatorFunc; validatorFunc = this.cache[i]; i++) {
+        var msg = validatorFunc()
+        if (msg) {
+            return msg
+        }
+    }
+}
